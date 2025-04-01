abd3c0442923870925e191fb46a20141252e919a
Galache Jacobo <jacobo.galache@ema.europa.eu>
2025-04-01 14:32:07 +0200

    logging added and fixed package location

--- startup_script.sh
+++ startup_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Starting deployment script"
 python -m ensurepip --upgrade
+echo "Installing pip..."
 python -m pip install --upgrade pip
+echo "Installing requirements..."
 python -m pip install -r requirements.txt
+echo "Starting application..."
 python hello_world.py
